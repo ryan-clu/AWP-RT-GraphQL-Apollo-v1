dc5bd5dc5bd0fed24dadce597237cb405f6d568d
Ryan Tang <ryan.tang.clu@gmail.com>
2022-06-09 14:53:34 -0400

    client side added

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Ryan's AWP project exploring GraphQL + Apollo Client + Apollo Server
+        </p>
+      </header>
+      <body>
+        <p>HELLO WORLD!</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
